51c3671b4df1741b10ac0af323bc17dcbbddb98e
Carmen Benitez <carlorben@gmail.com>
2020-05-27 11:30:29 -0400

    Basic website customization.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -17,7 +17,7 @@
  */
 function addRandomGreeting() {
   const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+      ['Gone Now by Bleachers', 'Malibu Nights by LANY', 'Melodrama by Lorde','After Hours by The Weeknd', 'American Teen by Khalid'];
 
   // Pick a random greeting.
   const greeting = greetings[Math.floor(Math.random() * greetings.length)];
