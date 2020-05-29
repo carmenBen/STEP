4c1114dc9c6b54d07bf2e399c36c660c18b5e570
Carmen Benitez <carlorben@gmail.com>
2020-05-29 17:07:21 -0400

    Added Miscellaneaous Fun page, complete with edited javascript code

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,17 +12,45 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/**
- * Adds a random greeting to the page.
+/*
+ * Adds a random activity car to the page.
  */
-function addRandomAlbum() {
-  const albums =
-      ['Gone Now by Bleachers', 'Malibu Nights by LANY', 'Melodrama by Lorde', 'After Hours by The Weeknd', 'American Teen by Khalid'];
+function addRandomActivity() {
+  const activityTitles = [
+    "Bake Banana Bread!",
+    "Listen to an Album!",
+    "Watch a RomCom!",
+    "Take a Nap!",
+    "Read a Book!"
+  ];
 
-  // Pick a random greeting.
-  const album = albums[Math.floor(Math.random() * albums.length)];
+  const activityDescriptions = [
+    "You've seen is all over instagram, now is the time to learn to bake banana bread! I recommend Bon Appetit's recipe(linked below), which I have been guilty of making multiple times in a week(enjoy the picture of my latest loaf below!).",
+    "Listen to an entire album. Yes, I mean start to finish! When will you have the chance to after this? Some recommendations include 'Fine Line' by Harry Styles, 'After Hours' by The Weeknd, and 'Melodrama' by Lorde.",
+    "A personal favorite choice, now is the time to start a group call with friends and watch (or rewatc!) a romcom. Some recommendations include 'The Half of It,' 'Someone Great,' 'Clueless,' and '10 Things I Hate About You.'",
+    "zzz",
+    "Yes, a real book, doesn't matter if it's paperback, hardcover, or an ebook. When was the last time you read one? I'm personally taking the time to reread the Harry Potter series.",
+  ];
 
+  const activityPhotos = [
+    "images/banana.JPG",
+    "images/music.JPG",
+    "images/movie.JPG",
+    "images/nap.JPG",
+    "images/reading.JPG",
+  ];
+
+  // Pick a random activity.
+  const index = Math.floor(Math.random() * activityTitles.length);
+  const activityTitle = activityTitles[index];
+  const activityDescription = activityDescriptions[index];
+  const activityPhoto = activityPhotos[index];
+  
   // Add it to the page.
-  const albumContainer = document.getElementById('album-container');
-  albumContainer.innerText = album;
+  const activityTitleContainer = document.getElementById("activity-title-container");
+  activityTitleContainer.innerText = activityTitle;
+  const activityDescriptionContainer = document.getElementById("activity-description-container");
+  activityDescriptionContainer.innerText = activityDescription;
+  const activityPhotoItem = document.getElementById("activity-photo");
+  activityPhotoItem.setAttribute("src",activityPhoto);
 }
